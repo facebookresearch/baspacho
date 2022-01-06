bd9699e7af5e8f781cbb7eda70b5c301fcf84bc4
Maurizio Monge <maurimo@fb.com>
2022-01-06 13:47:42 +0000

    gitignore, forgotten utils file

---
+++ Utils.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <vector>
+
+template<typename T>
+bool isStrictlyIncreasing(const std::vector<T>& v, size_t begin, size_t e) {
+    size_t i = begin + 1;
+    while(i < e && (v[i] > v[i-1])) {
+        i++;
+    }
+    return i == e;
+}
+
+template<typename T>
+bool isWeaklyIncreasing(const std::vector<T>& v, size_t begin, size_t e) {
+    size_t i = begin + 1;
+    while(i < e && (v[i] >= v[i-1])) {
+        i++;
+    }
+    return i == e;
+}
